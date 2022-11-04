094d3bf7424e95b24b2043bb0c176febe4660403
ZJ971005 <117420032+ZJ971005@users.noreply.github.com>
2022-11-04 11:34:03 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '皖情宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o05EI6Agl3Ll6jP2KTfCHKdWAgz4',
+      id: 'o05EI6IsvXd77buhz2Jdspyhkn7M',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
